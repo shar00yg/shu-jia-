06310d2e431f2d8cc5b5c31984df2470a94bf16a
shar00yg <yu1453010977@foxmail.com>
2019-07-22 13:55:17 +0800

    4

--- demo.js
+++ demo.js
@@ -2,3 +2,10 @@
 // file system 这个模块专门用来对系统文件的增删改查各种操作
 
 const fs =require('fs');
+fs.writeFile('./hello.txt',"hello node",(err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("数据写入成功");
+    }
+})
