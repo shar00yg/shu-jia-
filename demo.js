b0bdf083fd66c381dc6cd9cc3d60592a2df6238c
shar00yg <yu1453010977@foxmail.com>
2019-07-22 14:02:20 +0800

    6

--- demo.js
+++ demo.js
@@ -2,6 +2,9 @@
 // file system 这个模块专门用来对系统文件的增删改查各种操作
 
 const fs =require('fs');
+//异步写入文件，如果文件不存在，则自动创建
+
+
 
 //异步写入文件
 fs.writeFile('./hello.txt',"hello node",(err)=>{
@@ -17,6 +20,6 @@ fs.writeFile('./hello.txt',"hello node",(err)=>{
 //同步的代买比异步的先执行
 // console.log('我是同步的代码');
 
-
-let result = fs.writeFileSync('./heaven.txt','我是 heaven');
-console.log(result);
+//同步写入文件
+// let result = fs.writeFileSync('./heaven.txt','我是 heaven');
+// console.log(result);
