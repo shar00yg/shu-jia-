cfc8f9dc89246cebc0f4f6093fe82f83320cb3cb
shar00yg <yu1453010977@foxmail.com>
2019-07-23 09:54:10 +0800

    723 createServer()

--- http.js
+++ http.js
@@ -1,5 +1,13 @@
-const http = require('http');
-const surl =new URL('https://www.shiguangkey.com/video/1346?videoId=65770&classId=4228&playback=1');
-console.log(surl);
-// debugger
-// db.close;
+// const http = require('http');
+// const surl =new URL('https://www.shiguangkey.com/video/1346?videoId=65770&classId=4228&playback=1');
+// console.log(surl);
+
+
+let server = http.createServer((request,response)=>{
+    console.log(request.url);
+    response.write('heloo world');
+    response.end();
+})
+
+debugger
+db.close;
