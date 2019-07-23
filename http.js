066bab034b414d3db70155ea379de75199280524
shar00yg <yu1453010977@foxmail.com>
2019-07-23 09:40:36 +0800

    723 URL('href)

--- http.js
+++ http.js
@@ -1,1 +1,5 @@
-const 
+const http = require('http');
+const surl =new URL('https://www.shiguangkey.com/video/1346?videoId=65770&classId=4228&playback=1');
+console.log(surl);
+// debugger
+// db.close;
