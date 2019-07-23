377f8ee1c44950af0af7538e0e4b32560aede2f9
shar00yg <yu1453010977@foxmail.com>
2019-07-23 10:16:20 +0800

    723 setHeader（）

--- http.js
+++ http.js
@@ -1,13 +1,17 @@
-// const http = require('http');
+ const http = require('http');
 // const surl =new URL('https://www.shiguangkey.com/video/1346?videoId=65770&classId=4228&playback=1');
 // console.log(surl);
 
 
 let server = http.createServer((request,response)=>{
     console.log(request.url);
-    response.write('heloo world');
+    //设置响应的头部信息
+    response.setHeader('content-type','text/html;charset=utf-8');
+    response.write('<h1>我是首页</h1>');
     response.end();
-})
+});
 
-debugger
-db.close;
+//监听端口
+server.listen(4000);
+// debugger
+// db.close;
