90a45bd4327bdafa9221f5d5bcd13277ea854f78
shar00yg <yu1453010977@foxmail.com>
2019-07-25 16:27:56 +0800

    jieshu

--- dingcan/app.js
+++ dingcan/app.js
@@ -2,5 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.get('/dingcan',(req,res)=>{
+    console.log(req.query);
+    res.send();
+})
 app.use(express.static('public'));
 app.listen(4000);
