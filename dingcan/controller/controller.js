b2ea43b1fa69b3b4933c96c1442a2927d50e953f
shar00yg <yu1453010977@foxmail.com>
2019-07-26 16:17:28 +0800

    controller.savaData()

--- dingcan/controller/controller.js
+++ dingcan/controller/controller.js
@@ -1,6 +1,19 @@
-
+exports.saveData = funciton(req,res)
+{
+    //获取手机号和食物
+    let {phone,food} =req.query;
+    file.saveData(phone,req.query,(msg)=>{
+        res.send(msg);
+    });
+}
 
 
 
 
 //显示所有的订单
+exports.showAll = funciton (req,res)
+{
+    file.getAll((fileList)=>{
+        console.log(fileList);
+    });
+}
